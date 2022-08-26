916a35d10774eff685913159db1b104d960cdaff
GuilhermeChuman <guilhermectroncon@gmail.com>
2022-08-26 18:23:21 -0300

    tela login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,67 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+
+import { HomeComponent } from './pages/home/home.component';
+
+
+const routes: Routes = [
+  //AUTH
+  { 
+    path: 'auth/login', 
+    component: LoginComponent 
+  },
+  { 
+    path: 'auth/signup', 
+    component: SignupComponent 
+  },
+
+  //PAGES
+  { 
+    path: 'pages/home', 
+    component: HomeComponent 
+  },
+
+  //STANDARD
+  { 
+    path: '**', 
+    redirectTo: 'auth/login'
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    MatInputModule,
     BrowserAnimationsModule,
     MatSidenavModule,
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+
+export class HomeComponent {
+
+  //@ViewChild('sidenav') sidenav: MatSidenav;
+
+}
