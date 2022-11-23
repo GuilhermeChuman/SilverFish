e9e28da10673d8b912e05521ded1058fc82f36da
GuilhermeChuman <guilhermectroncon@gmail.com>
2022-11-23 01:57:17 -0300

    updating environment

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,37 +9,37 @@ export class APIService {
     }
 
     async login(data:any){
-        return await this.http.post(`${ environment.api_url_local }${environment.auth_login}`,data).toPromise().then(resp =>{
+        return await this.http.post(`${ environment.api_url }${environment.auth_login}`,data).toPromise().then(resp =>{
             return resp;
         });
     }
 
     async getAll(route:any){
-        return await this.http.get(`${ environment.api_url_local }${route}`).toPromise().then(resp =>{
+        return await this.http.get(`${ environment.api_url }${route}`).toPromise().then(resp =>{
             return resp;
         });
     }
 
     async add(route:any, data:any){
-        return await this.http.post(`${ environment.api_url_local }${route}`,data).toPromise().then(resp =>{
+        return await this.http.post(`${ environment.api_url }${route}`,data).toPromise().then(resp =>{
             return resp;
         });
     }
 
     async postWithId(route:any, data:any, id:any){
-        return await this.http.post(`${ environment.api_url_local }${route}${id}`,data).toPromise().then(resp =>{
+        return await this.http.post(`${ environment.api_url }${route}${id}`,data).toPromise().then(resp =>{
             return resp;
         });
     }
 
     async edit(route:any, id:any, data:any){
-        return await this.http.put(`${ environment.api_url_local }${route}${id}`, data).toPromise().then(resp =>{
+        return await this.http.put(`${ environment.api_url }${route}${id}`, data).toPromise().then(resp =>{
             return resp;
         });
     }
 
     async delete(route:any, id:any){
-        return await this.http.delete(`${ environment.api_url_local }${route}${id}`).toPromise().then(resp =>{
+        return await this.http.delete(`${ environment.api_url }${route}${id}`).toPromise().then(resp =>{
             return resp;
         });
     }
